da18f0baf2c919f80212827b949d8fcff8b2ac15
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-12-12 22:18:21 +0530

    feat : sample api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import harvestlyLogo from './assets/logo-white.svg'
 import './App.css'
-
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [user, setUser] = useState("Demo")
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:4000/api');
+      const json = await response.json();
+      setUser(json.user);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className='homepage'>
-          <h1>Welcome to Harvestly!</h1>
+          <h1>Welcome to Harvestly, {user}!</h1>
           <img src={harvestlyLogo} className="logo" alt="Harvestly logo"/>
       </div>
       
