db69be2e32b062c6d9c3aa18a9751facbc6931f7
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-12-13 23:22:15 +0530

    feat : connected to api endpt

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import harvestlyLogo from './assets/logo-white.svg'
 import './App.css'
 import { useEffect, useState } from 'react'
+const url =  import.meta.env.VITE_API_URL
 
 function App() {
   const [user, setUser] = useState("Demo")
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('http://localhost:4000/api');
+      const response = await fetch(url + '/api');
       const json = await response.json();
       setUser(json.user);
     };
@@ -17,7 +18,7 @@ function App() {
   return (
     <>
       <div className='homepage'>
-          <h1 className="text-6xl font-bold mt-5">Welcome to Harvestly, {user}!</h1>
+          <h1 className="text-6xl font-bold mt-5 mb-5">Welcome to Harvestly, {user}!</h1>
           <img src={harvestlyLogo} className="logo" alt="Harvestly logo"/>
       </div>
       
