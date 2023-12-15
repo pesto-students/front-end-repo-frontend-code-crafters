9562a25fac16e1d161d07f538677d043671cf456
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2023-12-16 00:02:35 +0530

    feat : login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import App from './App.jsx'
-import SignIn from './pages/SignIn.jsx';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-            <Route path="/" element={<App />}>
-            </Route>
-            <Route path="/signin" element={<SignIn />}>
-            </Route>
-            {/* // <Route path="/signup" element={<SignUp />}>
-            // </Route> */}
+        <Route path="/" element={<App />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-const VITE_API_URL = import.meta.env.VITE_API_URL
-const SignIn = () => {
+import { Link } from "react-router-dom";
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+const Login = () => {
   const googleAuth = () => {
-    window.open(
-      `${VITE_API_URL}/auth/google/callback`,
-      "_self"
-    );
+    window.open(`${VITE_API_URL}/auth/google/callback`, "_self");
   };
 
   return (
@@ -21,14 +19,17 @@ const SignIn = () => {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Login to your account
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Email address
               </label>
               <div className="mt-2">
@@ -45,11 +46,17 @@ const SignIn = () => {
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
                   Password
                 </label>
                 <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
                     Forgot password?
                   </a>
                 </div>
@@ -68,31 +75,38 @@ const SignIn = () => {
 
             <div>
               <div className="mb-2s">
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md mb-2 bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                Sign in
-              </button>
+                  Login
+                </button>
               </div>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Start a 14 day free trial
+          <div className="flex justify-center mb-2">
+            <span className="block text-sm font-medium leading-6 text-gray-900">Or</span>
+          </div>
+          <div class="flex items-center justify-center">
+            <button class="px-3 py-1.5 border flex gap-2 border-slate-200 rounded-lg text-gray-900 text-sm font-semibold leading-6 hover:border-slate-400 hover:shadow transition duration-150" onClick={googleAuth}>
+              <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"/>
+              <span>Login with Google</span>
+            </button>
+          </div>
+          
+          <p className="mt-5 text-center text-sm text-gray-500">
+            No account? No problem. {"  "}
+            <a
+              href="/register"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Create one!
             </a>
           </p>
-              <div>
-              <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={googleAuth}>
-                <span>Sign In with Google</span>
-              </button>
-              </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SignIn
+export default Login;
