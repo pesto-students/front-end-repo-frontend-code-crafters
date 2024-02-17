60e71c7f62d3ec216fab97e074cd2b64178fcc73
Pranav Srisrimal <pranavsrisrimal@gmail.com>
2024-02-17 15:38:21 +0530

    feat : login route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,7 +32,7 @@ const Login = (props) => {
     e.preventDefault();
     try {
       dispatch(loginStart());
-      const url = `${VITE_API_URL}/login`;
+      const url = `${VITE_API_URL}/user/login`;
       const res = await fetch(url, {
         method: "POST",
         headers: {
